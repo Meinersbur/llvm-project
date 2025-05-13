643843422e714c3b3bbabb8b58a6d3bb78cec661
Michael Kruse <llvm-project@meinersbur.de>
2025-05-13 15:42:32 +0200

    Fix intrinsic-module-path.f90

--- flang/test/Driver/intrinsic-module-path.f90
+++ flang/test/Driver/intrinsic-module-path.f90
@@ -1,4 +1,8 @@
 ! Ensure argument -fintrinsic-modules-path works as expected.
+! The driver will add the builtin search path to the -fc1 command, in addition to the -fintrinsic-modules-path provided by the user.
+! For regression tests, the default -fintrinsic-modules-path is provided by %flang_fc1.
+
+
 ! WITHOUT the option, the default location for the module is checked and no error generated.
 ! With the option GIVEN, the module with the same name is PREPENDED, and considered over the
 ! default one, causing a CHECKSUM error.
@@ -6,12 +10,18 @@
 !-----------------------------------------
 ! FRONTEND FLANG DRIVER (flang -fc1)
 !-----------------------------------------
-! RUN: %flang_fc1 -fsyntax-only %s  2>&1 | FileCheck %s --allow-empty --check-prefix=WITHOUT
-! RUN: not %flang_fc1 -fsyntax-only -fintrinsic-modules-path %S/Inputs/ %s  2>&1 | FileCheck %s --check-prefix=GIVEN
-! RUNx: %flang_fc1 -fsyntax-only -fintrinsic-modules-path %S/Inputs/ %s  2>&1 | FileCheck %s --check-prefix=GIVEN
-! RUNx: %flang_fc1 -fsyntax-only -fintrinsic-modules-path %S/Inputs/ %s  2>&1 > %S/out.log
-! RUNx: FileCheck %s --check-prefix=GIVEN --input-file=%S/out.log
-! RUNx: false
+! RUN: not %flang -fc1 -fsyntax-only %s  2>&1 | FileCheck %s --allow-empty --check-prefix=NONE
+! RUN: %flang_fc1  -fsyntax-only %s  2>&1 | FileCheck %s --allow-empty --check-prefix=WITHOUT
+! RUN: %flang -fsyntax-only %s  2>&1 | FileCheck %s --allow-empty --check-prefix=WITHOUT
+
+! RUN: not %flang -fc1 -fsyntax-only -fintrinsic-modules-path %S/Inputs/ %s  2>&1 | FileCheck %s --allow-empty --check-prefix=GIVEN
+! RUN: %flang_fc1  -fsyntax-only -fintrinsic-modules-path %S/Inputs/ %s  2>&1 | FileCheck %s --allow-empty --check-prefix=WITHOUT
+! RUN: not %flang -fsyntax-only -fintrinsic-modules-path %S/Inputs/ %s  2>&1 | FileCheck %s --allow-empty --check-prefix=GIVEN
+
+
+
+! NONE: 'ieee_arithmetic.mod' was not found
+! NONE: 'iso_fortran_env.mod' was not found
 
 ! WITHOUT-NOT: 'ieee_arithmetic.mod' was not found
 ! WITHOUT-NOT: 'iso_fortran_env.mod' was not found
